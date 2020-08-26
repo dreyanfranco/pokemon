81e5884c231ae4c48309b46d7a3e8d74d076017a
dreyanfranco <dreyanfranco@gmail.com>
2020-08-26 20:18:06 +0200

    second commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ function render(){
     // guardo en la variable container el acceso al id de pokemon que esta dentro de div
 let container = document.getElementById('pokemon');
 // acceso a la variable para decirle de donde viene la imagen. Cambiando el numero escojo el pokemon.
-container.innerHTML = `<img src="https://tinyurl.com/ironhack-pokemons/${count}.svg">`;
+container.innerHTML = `<img class="poke" src="https://tinyurl.com/ironhack-pokemons/${count}.svg">`;
 
 let prev = document.getElementById('prev');
 let next = document.getElementById('next');
