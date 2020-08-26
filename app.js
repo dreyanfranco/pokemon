9a63ef1c5beea6bae35c72123fdee443dab669e4
dreyanfranco <dreyanfranco@gmail.com>
2020-08-26 20:05:21 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+let count = 1;
+function render(){
+    // guardo en la variable container el acceso al id de pokemon que esta dentro de div
+let container = document.getElementById('pokemon');
+// acceso a la variable para decirle de donde viene la imagen. Cambiando el numero escojo el pokemon.
+container.innerHTML = `<img src="https://tinyurl.com/ironhack-pokemons/${count}.svg">`;
+
+let prev = document.getElementById('prev');
+let next = document.getElementById('next');
+
+prev.onclick = function(){
+    if (count > 1) {
+        count = count - 1;
+        render();
+    }
+    
+}
+
+next.onclick = function(){
+    if (count < 650){
+        count = count + 1;
+        render();
+    }
+    
+}
+}
+render();
